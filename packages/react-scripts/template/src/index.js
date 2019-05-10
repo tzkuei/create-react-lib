f8b18e98fecdde0fbe5e4f560e959f1017fb9e40
Ting Kuei <ting@kuei.com>
2019-05-10 22:05:14 +0100

    Added sample lib

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import _ from 'lodash';
+import numRef from './ref.json';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export function numToWord(num) {
+  return _.reduce(
+    numRef,
+    (accum, ref) => {
+      return ref.num === num ? ref.word : accum;
+    },
+    ''
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export function wordToNum(word) {
+  return _.reduce(
+    numRef,
+    (accum, ref) => {
+      return ref.word === word && word.toLowerCase() ? ref.num : accum;
+    },
+    -1
+  );
+}
